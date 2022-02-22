120853527b546703cf924d29b5707a74b6d4a6ad
mia <90076@naver.com>
2022-02-22 16:55:34 +0900

    emailCheck

--- controllers/emailCheck.js
+++ controllers/emailCheck.js
@@ -4,18 +4,18 @@ const connection = require("../app");
 
 const checkValidEmail = function (req, res, next) {
   //1. 이미 존재하는 이메일인가?
+  const emailQuery = `SELECT EXISTS(SELECT * FROM user_info WHERE id='`;
+  const id = req.body.data;
+  console.log(id);
+  let isValidEmail = false;
   connection.query(emailQuery + `${id}')`, (err, rows, fields) => {
     if (err) {
       throw err;
     }
     const key = Object.keys(rows[0])[0];
-    console.log(rows);
     rows[0][key] == 0 ? (isValidEmail = true) : (isValidEmail = false);
-    console.log(isValidEmail);
-    console.log("checking Before " + isValidEmail); //실행순서 2
+    res.json({ result: isValidEmail });
   });
-  console.log(isValidEmail);
-  next(isValidEmail);
 };
 
 module.exports = {
--- routes/emailCheck.js
+++ routes/emailCheck.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const controller = require("../controllers/emailCheck");
 
 //1. 이메일 유효 확인
-router.post("/emailCheck", controller.checkValidEmail);
+router.post("/", controller.checkValidEmail);
 
 module.exports = router;
