2006fa8ccba2a1963324e6f50d30664571ad230d
mia <90076@naver.com>
2022-03-02 17:53:43 +0900

    update pw, salt

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,6 +3,34 @@
 const { user_info, user_encrypt } = require("../models/index");
 const crypto = require("crypto");
 
+function findPW(id) {
+  return user_encrypt
+    .findAll({
+      attributes: ["pw"],
+      where: { id: id },
+    })
+    .then((results) => {
+      return results[0].dataValues.pw;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function findSalt(id) {
+  return user_encrypt
+    .findAll({
+      attributes: ["salt"],
+      where: { id: id },
+    })
+    .then((results) => {
+      return results[0].dataValues.salt;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
 /**
  * 1. 비밀번호 확인 전, 아이디 존재 확인
  * @param {*} req
@@ -12,7 +40,6 @@ const crypto = require("crypto");
 const checkID = (req, res, next) => {
   user_info
     .findOne({
-      attribute: ["id"],
       where: { id: req.body.id },
     })
     .then((results) => {
@@ -32,22 +59,11 @@ const checkID = (req, res, next) => {
 const checkPW = async (req, res, next) => {
   console.log("등록된 아이디");
 
-  let hashPassword, salt;
   /**
    * 1) user_encrypt 내 저장된 해시 비밀번호와 솔트 찾기
    */
-  await user_encrypt
-    .findAll({
-      attributes: ["pw", "salt"],
-      where: { id: req.body.id },
-    })
-    .then((results) => {
-      hashPassword = results[0].dataValues.pw;
-      salt = results[0].dataValues.salt;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  const hashPassword = await findPW(req.body.id);
+  const salt = await findSalt(req.body.id);
 
   /**
    * 2) 연산을 통해 req.body.pw와 hashPassword의 일치 여부 확인
@@ -60,42 +76,54 @@ const checkPW = async (req, res, next) => {
     }
 
     const hash = key.toString("base64"); //사용자가 입력한 패스워드를 salt를 통해 sha512로 암호화한 패스워드
-    hashPassword === hash
-      ? res.json({ result: true })
-      : res.json({ result: false });
+    hashPassword === hash ? next() : res.json({ result: false });
   });
+};
 
-  //////salt 변경할 것!!
-  // crypto.randomBytes(32, (err, buf) => {
-  //   if (err) {
-  //     throw err;
-  //   }
+/**
+ * 3. user_encrypt 테이블
+ * - pw, salt 값 변경
+ */
+const updateTable = (req, res, next) => {
+  const id = req.body.id;
+  const pw = req.body.pw;
 
-  //   const newSalt = buf.toString("base64");
+  crypto.randomBytes(32, (err, buf) => {
+    if (err) {
+      throw err;
+    }
 
-  //   /**
-  //    *  평문 pw, salt, iteration, byte length, digest(암호화) method, callback 함수
-  //    */
-  //   crypto.pbkdf2(pw, newSalt, 256, 32, "sha512", (err, key) => {
-  //     if (err) {
-  //       throw err;
-  //     }
+    const newSalt = buf.toString("base64");
 
-  //     const newHashPassword = key.toString("base64"); //salt와 암호화된 최종 pw
+    /**
+     *  평문 pw, salt, iteration, byte length, digest(암호화) method, callback 함수
+     */
+    crypto.pbkdf2(pw, newSalt, 256, 32, "sha512", (err, key) => {
+      if (err) {
+        throw err;
+      }
 
-  //     user_encrypt
-  //       .create({ No: No, id: req.body.id, pw: newHashPassword, salt: newSalt })
-  //       .then((results) => {
-  //         console.log(results);
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //       });
-  //   });
-  // });
+      const newHashPassword = key.toString("base64"); //salt와 암호화된 최종 pw
+
+      user_encrypt
+        .update(
+          { pw: newHashPassword, salt: newSalt },
+          {
+            where: { id: id },
+          }
+        )
+        .then((results) => {
+          res.json({ result: true });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+  });
 };
 
 module.exports = {
   checkID,
   checkPW,
+  updateTable,
 };
--- routes/signin.js
+++ routes/signin.js
@@ -4,6 +4,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/signin");
 
-router.post("/", controller.checkID, controller.checkPW);
+router.post(
+  "/",
+  controller.checkID,
+  controller.checkPW,
+  controller.updateTable
+);
 
 module.exports = router;
