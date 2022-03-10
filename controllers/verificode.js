71013bc0def583f77d17d3e9657407fe68e3db4d
mia <90076@naver.com>
2022-03-10 15:34:59 +0900

    nodemailer(2)

--- controllers/verificode.js
+++ controllers/verificode.js
@@ -1,45 +1,75 @@
 "use strict";
 
 const nodemailer = require("nodemailer");
+const redis = require("redis");
+const sanitizeHtml = require("sanitize-html");
+require("dotenv").config();
 
+/**
+ * 랜덤 인증번호 제작
+ * - 4자리 임의의 정수 반환
+ */
 function getRandomInt() {
-  //1000<= <10000 사이의 임의의 정수 반환
   return Math.floor(Math.random() * (10000 - 1000)) + 1000;
 }
 
 /**
- * SMTP 정보
+ * redis 연결
  */
-const email = {
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  secure: false,
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT),
+});
+client.connect();
+
+/**
+ * transporter 객체
+ * - SMTP 정보
+ */
+const transporter = {
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT),
+  secure: JSON.parse(process.env.SMTP_SECURE),
   auth: {
-    user: "3a44f854f50f94",
-    pass: "57eefd79dec279",
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PW,
   },
 };
 
+/**
+ * 전송할 이메일 정보 제작
+ * @param {} to id
+ * @returns email_data
+ */
 function makeData(to) {
   const code = getRandomInt();
+  client.set(`${to}`, code); //암호화?
+
   return {
     from: "administrator@gmail.com",
     to: `${to}`,
-    subject: "테스트 메일입니다.",
+    subject: "이메일 인증번호 전송 메일입니다.",
     text: `인증번호는 [ ${code} ]입니다.`,
   };
 }
 
-const send = function (req, res, next) {
-  const email_data = makeData(req.body.id);
+const send = async function (req, res, next) {
+  const id = sanitizeHtml(req.body.id);
+  const email_data = makeData(id);
 
-  nodemailer.createTransport(email).sendMail(email_data, (err, info) => {
+  //인증번호 발송
+  nodemailer.createTransport(transporter).sendMail(email_data, (err, info) => {
     if (err) {
       console.log(err);
     }
-    console.log(info);
-    res.json({ result: true });
+    // console.log(info);
   });
+
+  ////////인증 완료시 확인할 것, 그리고 데이터 낭비를 위해 삭제
+  const val = await client.get(`${id}`, redis.print);
+  console.log(val);
+  ////////////////
+  res.json({ code: val });
 };
 
 module.exports = {
