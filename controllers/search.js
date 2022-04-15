21050dd8417464aaac492e20d6170e6cb2f5d4a9
mia <90076@naver.com>
2022-04-15 17:34:04 +0900

    direct-search-btn(2)

--- controllers/search.js
+++ controllers/search.js
@@ -3,30 +3,75 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const sanitizeHTML = require("sanitize-html");
+const puppeteer = require("puppeteer");
+const { json } = require("express");
 
-const getHTML = async (word) => {
+//첫 번째 페이지
+const getHTML = async (title) => {
   try {
-    const html = await axios.get(
-      `https://movie.naver.com/movie/search/result.naver?query=${encodeURI(
-        word
-      )}&section=all&ie=utf8`
+    return await axios.get(
+      `https://movie.naver.com/movie/search/result.naver?section=movie&query=${encodeURI(
+        title
+      )}&page=1`
     );
-    return html;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//다음 페이지..
+const pageHTML = async (count, title) => {
+  try {
+    return await axios.get(
+      `https://movie.naver.com/movie/search/result.naver?section=movie&query=${encodeURI(
+        title
+      )}&page=${count}`
+    );
   } catch (err) {
     console.log(err);
   }
 };
 
 const searchWord = async (req, res, next) => {
-  const html = await getHTML(sanitizeHTML(req.body.word));
+  let jsonArray = new Array(); //객체들을 담아서 보낼 배열
+  const title = sanitizeHTML(req.body.word);
+  const html = await getHTML(title);
   const $ = cheerio.load(html.data);
-  const movies = $(".search_list_1").children("li");
-  let images = [];
-  movies.each((idx, movie) => {
-    const img = $(movie).find("p a img").attr("src");
-    images.push(img);
-  });
-  res.json({ images: images });
+
+  const text = $(".search_header .num").text().replace(/ /g, "");
+  const var1 = text.replace(/ /g, "").indexOf("/");
+  const var2 = text.replace(/ /g, "").indexOf("건");
+  const count = parseInt(text.substring(var1 + 1, var2));
+  const totalPage = parseInt(count / 10) + 1;
+
+  for (let i = 1; i <= totalPage; i++) {
+    const html = await pageHTML(i, title);
+    const $ = cheerio.load(html.data);
+    const movies = $(".search_list_1").first().children("li");
+
+    //한 페이지 내 영화 리스트
+    for (const movie of movies) {
+      let json = new Object();
+      //1. 영화 상세 페이지 링크
+      json.link =
+        "https://movie.naver.com" + $(movie).find("dl dt a").attr("href");
+      // const browser = await puppeteer.launch({ headless: true });
+      // const page = await browser.newPage();
+      // await page.goto(`https://movie.naver.com${link}`);
+      // const src = await page.$eval(".poster > a > img", (ele) => ele.src);
+      const smallImgSrc = $(movie).find("p > a > img").attr("src");
+      const typePos = $(movie).find("p > a > img").attr("src").indexOf("type");
+
+      //2. 영화 이미지 링크
+      json.imgSrc = smallImgSrc.slice(undefined, typePos);
+
+      //3. 영화 토픽
+      json.title = $(movie).find("dl dt").text();
+      jsonArray.push(json);
+    }
+  }
+  console.log(jsonArray.length);
+  res.json({ movies: jsonArray });
 };
 
 module.exports = {
